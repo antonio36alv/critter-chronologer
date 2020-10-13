d0f7536724e448872c3ac2ed8ff047af1ecf86a0
antonio36alv <ant.36alv@gmail.com>
2020-10-13 12:37:48 -0400

    original layout of data

--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/pet/Pet.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/pet/Pet.java
@@ -1,5 +1,43 @@
 package com.udacity.jdnd.course3.critter.pet;
 
+import com.udacity.jdnd.course3.critter.user.Customer;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.annotations.Nationalized;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@NoArgsConstructor
+@RequiredArgsConstructor
 public class Pet {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(length = 50)
+    @NotNull
+    private String type;
+
+    @Nationalized
+    @Column(length = 50)
+    @NotNull
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    @NotNull
+    private Customer customer;
+    // many to one with owner referenced by owner id
+
+    @NotNull
+    private LocalDateTime birthDate;
+
+    @Column(length = 500)
+    private String notes;
 }
