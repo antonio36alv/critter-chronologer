3de0a8fabafe37848b9caa4dc6453ca923e95897
antonio36alv <ant.36alv@gmail.com>
2020-10-13 15:34:32 -0400

    removed lombok usage

--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/pet/PetNotFoundException.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/pet/PetNotFoundException.java
@@ -2,14 +2,16 @@ package com.udacity.jdnd.course3.critter.pet;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 @Data
-@NoArgsConstructor
 public class PetNotFoundException extends RuntimeException {
-    /*
-    Default constructor handled by lombok
-     */
+
+    public PetNotFoundException() {
+    }
+
     public PetNotFoundException(String message) {
         super(message);
     }
+
 }
