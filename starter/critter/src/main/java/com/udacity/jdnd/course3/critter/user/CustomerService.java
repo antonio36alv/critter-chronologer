df2910398c33ad34f1c635f7eed88aa359993762
antonio36alv <ant.36alv@gmail.com>
2020-10-13 23:24:20 -0400

    implemented findById and save

--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/user/CustomerService.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/user/CustomerService.java
@@ -1,4 +1,9 @@
 package com.udacity.jdnd.course3.critter.user;
 
 public interface CustomerService {
+
+    Customer findById(Long id);
+
+    Customer save(Customer customer);
+
 }
--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/user/CustomerServiceImpl.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/user/CustomerServiceImpl.java
@@ -1,7 +1,24 @@
 package com.udacity.jdnd.course3.critter.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class CustomerServiceImpl implements CustomerService {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Override
+    public Customer findById(Long id) {
+        Optional<Customer> customer = Optional.ofNullable(customerRepository.findById(id).orElseThrow(CustomerNotFoundException::new));
+        return customer.get();
+    }
+
+    @Override
+    public Customer save(Customer customer) {
+        return customerRepository.save(customer);
+    }
 }
