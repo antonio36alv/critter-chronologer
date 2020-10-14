029bf1f49b83c81dbb0144e26c8fa19814104fee
antonio36alv <ant.36alv@gmail.com>
2020-10-13 23:23:02 -0400

    configured relationship with pets

--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/user/Customer.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/user/Customer.java
@@ -1,11 +1,14 @@
 package com.udacity.jdnd.course3.critter.user;
 
+import com.udacity.jdnd.course3.critter.pet.Pet;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
+import java.util.List;
 
 @Data
 @Entity
@@ -15,5 +18,8 @@ public class Customer extends User {
 
     @NotNull
     private String phoneNumber;
+
+    @OneToMany(mappedBy = "customer")
+    private List<Pet> pet;
 
 }
