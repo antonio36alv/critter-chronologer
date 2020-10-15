a3da9113f42eb89c5780fef4e761c2a91f11eeb1
antonio36alv <ant.36alv@gmail.com>
2020-10-14 21:21:39 -0400

    added find all method

--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/user/CustomerService.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/user/CustomerService.java
@@ -1,8 +1,12 @@
 package com.udacity.jdnd.course3.critter.user;
 
+import java.util.List;
+
 public interface CustomerService {
 
     Customer findById(Long id);
+
+    List<Customer> findAll();
 
     Customer save(Customer customer);
 
