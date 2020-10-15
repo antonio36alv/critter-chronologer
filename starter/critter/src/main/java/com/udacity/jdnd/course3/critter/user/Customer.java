d7210a0317bcc2b0e323e746e979138ed3477fbb
antonio36alv <ant.36alv@gmail.com>
2020-10-14 21:11:38 -0400

    added cascade type all, made pets plural

--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/user/Customer.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/user/Customer.java
@@ -5,21 +5,20 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.util.List;
 
-@Data
-@Entity
 //@NoArgsConstructor
 //@RequiredArgsConstructor
+@Data
+@Entity
 public class Customer extends User {
 
     @NotNull
     private String phoneNumber;
 
-    @OneToMany(mappedBy = "customer")
-    private List<Pet> pet;
+    @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL)
+    private List<Pet> pets;
 
 }
