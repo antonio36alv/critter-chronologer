998312e5abeb8dda66d2021989fcf437917e7a1e
antonio36alv <ant.36alv@gmail.com>
2020-10-16 16:40:39 -0400

    implemented save and findAll

--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/schedule/ScheduleService.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/schedule/ScheduleService.java
@@ -1,4 +1,11 @@
 package com.udacity.jdnd.course3.critter.schedule;
 
+import java.util.List;
+
 public interface ScheduleService {
+
+    ScheduleDTO save(ScheduleDTO scheduleDTO);
+
+    List<ScheduleDTO> findAll();
+
 }
