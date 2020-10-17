1bf45eca6c425ce6ba79f2af517a156def7f7abf
antonio36alv <ant.36alv@gmail.com>
2020-10-17 18:12:16 -0400

    setup data and relationships

--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/schedule/Schedule.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/schedule/Schedule.java
@@ -1,22 +1,34 @@
 package com.udacity.jdnd.course3.critter.schedule;
 
+import com.udacity.jdnd.course3.critter.pet.Pet;
+import com.udacity.jdnd.course3.critter.user.Employee;
+import com.udacity.jdnd.course3.critter.user.EmployeeSkill;
 import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Set;
 
 @Data
 @Entity
-//@NoArgsConstructor
-//@RequiredArgsConstructor
 public class Schedule {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-//    private List<Long> empl
+    @ManyToMany
+    private List<Pet> pets;
+
+    @ManyToMany
+    private List<Employee> employees;
+
+    @NotNull
+    private LocalDate date;
+
+    @ElementCollection(targetClass = EmployeeSkill.class)
+    private Set<EmployeeSkill> activities;
+
 }
--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/user/Employee.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/user/Employee.java
@@ -1,21 +1,22 @@
 package com.udacity.jdnd.course3.critter.user;
 
+import com.udacity.jdnd.course3.critter.schedule.Schedule;
 import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 
-import javax.persistence.Column;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
 import java.util.List;
 import java.util.Set;
 
 @Data
 @Entity
-//@NoArgsConstructor
 public class Employee extends User {
 
     @ElementCollection(targetClass = EmployeeSkill.class)
     private Set<EmployeeSkill> skills;
+
+    @ManyToMany
+    private List<Schedule> schedule;
 
 }
